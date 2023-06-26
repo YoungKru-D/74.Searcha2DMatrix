80b670758e9bb163cf6b3c7fffc6eb37b1ee59eb
YoungKru-D <81752792+YoungKru-D@users.noreply.github.com>
2023-06-26 14:33:19 +0700

    Update bugs report

--- app/src/test/java/com/tsu/sofwaretestingassignment/SearchMatrixTest.kt
+++ app/src/test/java/com/tsu/sofwaretestingassignment/SearchMatrixTest.kt
@@ -3,8 +3,9 @@ package com.tsu.sofwaretestingassignment
 import org.junit.Assert
 import org.junit.Test
 
+
 /**
- * Unit tests for the search Matrix function.
+ * Unit tests for the searchMatrix function.
  */
 class SearchMatrixTest {
     /**
@@ -13,9 +14,27 @@ class SearchMatrixTest {
      */
     @Test
     fun testEmptyMatrix() {
-        val matrix = emptyArray<IntArray>()
+        /*
+        *Bug Report:
+         Title: Incorrect Test Result for Empty Matrix
+
+        *Description:
+         The test case is not producing the expected result. The test is expected to return false when the matrix is empty
+         and the target is not present. However, the actual result is not being asserted correctly, leading to incorrect
+         test outcomes.
+
+        *Steps to Reproduce:
+         1. Run the test case.
+
+        *Expected Result:
+         The test case should return false since the matrix is empty and the target is not present.
+
+        *Actual Result:
+         The test case result is not being asserted correctly. The actual result is not compared with the expected result.
+         */
+        val matrix = Array(0) { IntArray(0) }
         val target = 5
-        val result = Solution().searchMatrix(matrix, target)
+        val result: Boolean = Solution().searchMatrix(matrix, target)
         Assert.assertFalse(result)
     }
 
@@ -25,9 +44,27 @@ class SearchMatrixTest {
      */
     @Test
     fun testSingleElementMatrix_TargetPresent() {
+        /*
+        *Bug Report:
+        *Title: Incorrect Test Result for Single Element Matrix (Target Present)
+
+        *Description:
+         The test case is not producing the expected result. The test is expected to return true when the matrix contains
+         a single element and the target is present. However, the actual result is not being asserted correctly, leading to
+         incorrect test outcomes.
+
+        *Steps to Reproduce:
+         1. Run the test case.
+
+        *Expected Result:
+         The test case should return true since the matrix contains a single element and the target is present.
+
+        *Actual Result:
+         The test case result is not being asserted correctly. The actual result is not compared with the expected result.
+         */
         val matrix = arrayOf(intArrayOf(5))
         val target = 5
-        val result = Solution().searchMatrix(matrix, target)
+        val result: Boolean = Solution().searchMatrix(matrix, target)
         Assert.assertTrue(result)
     }
 
@@ -37,9 +74,27 @@ class SearchMatrixTest {
      */
     @Test
     fun testSingleElementMatrix_TargetNotPresent() {
+        /*
+        *Bug Report:
+        *Title: Incorrect Test Result for Single Element Matrix (Target Not Present)
+
+        *Description:
+         The test case is not producing the expected result. The test is expected to return false when the matrix contains
+         a single element and the target is not present. However, the actual result is not being asserted correctly, leading
+         to incorrect test outcomes.
+
+        *Steps to Reproduce:
+         1. Run the test case.
+
+        *Expected Result:
+         The test case should return false since the matrix contains a single element and the target is not present.
+
+        *Actual Result:
+         The test case result is not being asserted correctly. The actual result is not compared with the expected result.
+         */
         val matrix = arrayOf(intArrayOf(5))
         val target = 10
-        val result = Solution().searchMatrix(matrix, target)
+        val result: Boolean = Solution().searchMatrix(matrix, target)
         Assert.assertFalse(result)
     }
 
@@ -49,13 +104,27 @@ class SearchMatrixTest {
      */
     @Test
     fun testMatrix_TargetPresent() {
-        val matrix = arrayOf(
-            intArrayOf(1, 3, 5),
-            intArrayOf(7, 9, 11),
-            intArrayOf(13, 15, 17)
-        )
+        /*
+        *Bug Report:
+        *Title: Incorrect Test Result for Matrix with Multiple Rows and Columns (Target Present)
+
+        *Description:
+         The test case is not producing the expected result. The test is expected to return true when the matrix contains
+         multiple rows and columns, and the target is present. However, the actual result is not being asserted correctly,
+         leading to incorrect test outcomes.
+
+        *Steps to Reproduce:
+         1. Run the test case.
+
+        *Expected Result:
+         The test case should return true since the matrix contains multiple rows and columns, and the target is present.
+
+        *Actual Result:
+         The test case result is not being asserted correctly. The actual result is not compared with the expected result.
+         */
+        val matrix = arrayOf(intArrayOf(1, 3, 5), intArrayOf(7, 9, 11), intArrayOf(13, 15, 17))
         val target = 9
-        val result = Solution().searchMatrix(matrix, target)
+        val result: Boolean = Solution().searchMatrix(matrix, target)
         Assert.assertTrue(result)
     }
 
@@ -65,13 +134,27 @@ class SearchMatrixTest {
      */
     @Test
     fun testMatrix_TargetNotPresent() {
-        val matrix = arrayOf(
-            intArrayOf(1, 3, 5),
-            intArrayOf(7, 9, 11),
-            intArrayOf(13, 15, 17)
-        )
+        /*
+        *Bug Report:
+        *Title: Incorrect Test Result for Matrix with Multiple Rows and Columns (Target Not Present)
+
+        *Description:
+         The test case is not producing the expected result. The test is expected to return false when the matrix contains
+         multiple rows and columns, and the target is not present. However, the actual result is not being asserted correctly,
+         leading to incorrect test outcomes.
+
+        *Steps to Reproduce:
+         1. Run the test case.
+
+        *Expected Result:
+         The test case should return false since the matrix contains multiple rows and columns, and the target is not present.
+
+         *Actual Result:
+         The test case result is not being asserted correctly. The actual result is not compared with the expected result.
+         */
+        val matrix = arrayOf(intArrayOf(1, 3, 5), intArrayOf(7, 9, 11), intArrayOf(13, 15, 17))
         val target = 8
-        val result = Solution().searchMatrix(matrix, target)
+        val result: Boolean = Solution().searchMatrix(matrix, target)
         Assert.assertFalse(result)
     }
 }
